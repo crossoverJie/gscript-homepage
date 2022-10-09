381c1fa8126931445a771059643321d883d2d284
Baozoulolw <Baozoulolw@gmail.com>
2022-10-09 16:04:51 +0800

    改变路由方式

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 
 const routes = [
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
